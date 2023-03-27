3daad0347a181aa730e50dcd8165825f64f2c02c
Wouter <wouter@thisisnota.studio>
2023-03-27 15:46:43 +0200

    Initial Commit

---
+++ Examples/ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesApp.swift
+//  Examples
+//
+//  Created by Wouter van de Kamp on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
